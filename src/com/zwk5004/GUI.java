cda3769d1e2df40da7d5481c60f17166662f2d4e
Zachery Kampmann <zwk5004@gmail.com>
2021-04-27 10:37:48 -0400

    Initial prototype

---
+++ src/com/zwk5004/GUI.java
@@ -0,0 +1,166 @@
+package com.zwk5004;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.MatteBorder;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.font.TextAttribute;
+import java.util.Map;
+
+public class GUI {
+    JFrame frame;
+    JLabel machineLabel, typeLabel, rackSizeLabel, numSamplesLabel, machineInfoLabel,
+            supportedTypesLabel, maxRacksLabel, maxSamplesLabel;
+    JComboBox<String> machineSelect, typeSelect, rackSizeSelect;
+    JTextField numSamplesInput;
+    JButton runButton, cancelButton;
+    JProgressBar progressBar1, progressBar2, progressBar3, progressBar4, progressBar5, progressBar6;
+    JLabel progressLabel1, progressLabel2, progressLabel3, progressLabel4, progressLabel5, progressLabel6;
+    boolean isRunning = false;
+
+    public GUI (){
+        frame = new JFrame();
+
+        machineLabel = new JLabel("Machine");
+        typeLabel = new JLabel("Type");
+        rackSizeLabel = new JLabel("Rack Size");
+        numSamplesLabel = new JLabel("Total Samples");
+        machineInfoLabel = new JLabel("Machine Info");
+        machineInfoLabel.setFont(new Font(null, Font.PLAIN, 16));
+        Font font = machineInfoLabel.getFont();
+        Map attributes = font.getAttributes();
+        attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+        machineInfoLabel.setFont(font.deriveFont(attributes));
+        supportedTypesLabel = new JLabel("Supported Types: DNA, RNA, mRNA");
+        maxRacksLabel = new JLabel("Max Racks: 20");
+        maxSamplesLabel = new JLabel("Max Samples: 2880");
+        runButton = new JButton("Run");
+        runButton.addActionListener(e -> {
+            isRunning = true;
+            this.runButton.setVisible(false);
+            this.cancelButton.setVisible(true);
+        });
+        cancelButton = new JButton("Cancel");
+        cancelButton.setVisible(false);
+        cancelButton.addActionListener(e -> {
+            isRunning = false;
+            this.cancelButton.setVisible(false);
+            this.runButton.setVisible(true);
+        });
+
+        String[] machines = new String[]{"HiSeq 2000", "HiSeq 3000", "MiSeq", "Sanger", "PacBio", "Ion Torrent"};
+        machineSelect = new JComboBox<>(machines);
+        String[] sequenceTypes = new String[]{"DNA", "RNA", "mRNA"};
+        typeSelect = new JComboBox<>(sequenceTypes);
+        String[] rackSizes = new String[]{"16", "32", "64", "100", "144", "288"};
+        rackSizeSelect = new JComboBox<>(rackSizes);
+        numSamplesInput = new JTextField("", 10);
+        numSamplesInput.setSize(100, 14);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+
+        JPanel headerPanel = new JPanel();
+        headerPanel.setLayout(new FlowLayout());
+        headerPanel.setBorder(new LineBorder(Color.BLACK));
+
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new BorderLayout());
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new FlowLayout());
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.TRAILING));
+
+        inputPanel.add(machineLabel);
+        inputPanel.add(machineSelect);
+        inputPanel.add(typeLabel);
+        inputPanel.add(typeSelect);
+        inputPanel.add(rackSizeLabel);
+        inputPanel.add(rackSizeSelect);
+        inputPanel.add(numSamplesLabel);
+        inputPanel.add(numSamplesInput);
+
+        buttonPanel.add(runButton);
+        buttonPanel.add(cancelButton);
+
+        topPanel.add(inputPanel, BorderLayout.PAGE_START);
+        topPanel.add(buttonPanel, BorderLayout.PAGE_END);
+
+        JPanel machineInfoPanel = new JPanel();
+        machineInfoPanel.setLayout(new BoxLayout(machineInfoPanel, BoxLayout.PAGE_AXIS));
+        machineInfoPanel.setBorder(new MatteBorder(0, 1, 0, 0, Color.BLACK));
+        machineInfoPanel.setBorder(BorderFactory.createCompoundBorder(new MatteBorder(0, 1, 0, 0, Color.BLACK),
+                new EmptyBorder(10,10,10,10)));
+
+        machineInfoPanel.add(machineInfoLabel);
+        machineInfoPanel.add(supportedTypesLabel);
+        machineInfoPanel.add(maxRacksLabel);
+        machineInfoPanel.add(maxSamplesLabel);
+
+        headerPanel.add(topPanel);
+        headerPanel.add(machineInfoPanel);
+
+        JPanel progressPanel = new JPanel();
+        progressPanel.setLayout(new BoxLayout(progressPanel, BoxLayout.PAGE_AXIS));
+        progressBar1 = createProgressBar();
+        progressBar2 = createProgressBar();
+        progressBar3 = createProgressBar();
+        progressBar4 = createProgressBar();
+        progressBar5 = createProgressBar();
+        progressBar6 = createProgressBar();
+        progressLabel1 = new JLabel("Rack 1", SwingConstants.LEADING);
+        progressLabel1.setFont(new Font(null, Font.PLAIN, 16));
+        progressLabel2 = new JLabel("Rack 2", SwingConstants.LEADING);
+        progressLabel2.setFont(new Font(null, Font.PLAIN, 16));
+        progressLabel3 = new JLabel("Rack 3", SwingConstants.LEADING);
+        progressLabel3.setFont(new Font(null, Font.PLAIN, 16));
+        progressLabel4 = new JLabel("Rack 4", SwingConstants.LEADING);
+        progressLabel4.setFont(new Font(null, Font.PLAIN, 16));
+        progressLabel5 = new JLabel("Rack 5", SwingConstants.LEADING);
+        progressLabel5.setFont(new Font(null, Font.PLAIN, 16));
+        progressLabel6 = new JLabel("Rack 6", SwingConstants.LEADING);
+        progressLabel6.setFont(new Font(null, Font.PLAIN, 16));
+
+        progressPanel.add(progressLabel1);
+        progressPanel.add(progressBar1);
+        progressPanel.add(progressLabel2);
+        progressPanel.add(progressBar2);
+        progressPanel.add(progressLabel3);
+        progressPanel.add(progressBar3);
+        progressPanel.add(progressLabel4);
+        progressPanel.add(progressBar4);
+        progressPanel.add(progressLabel5);
+        progressPanel.add(progressBar5);
+        progressPanel.add(progressLabel6);
+        progressPanel.add(progressBar6);
+
+        mainPanel.add(headerPanel, BorderLayout.PAGE_START);
+        mainPanel.add(progressPanel, BorderLayout.CENTER);
+
+        frame.add(mainPanel);
+
+        frame.setSize(new Dimension(1000, 1000));
+        frame.setVisible(true);
+    }
+
+    private JProgressBar createProgressBar() {
+        JProgressBar tempBar = new JProgressBar(0, 100);
+        tempBar.setValue(((int) (Math.random() * 100)));
+        tempBar.setStringPainted(true);
+        return tempBar;
+    }
+}
---
+++ src/com/zwk5004/Main.java
@@ -0,0 +1,8 @@
+package com.zwk5004;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    new GUI();
+    }
+}
